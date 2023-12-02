6845e664d24393adf36494db449c84645e36f9b7
JudgementMaxim <maxim.skalenko@gmail.com>
2023-12-02 23:07:31 +0100

    Bot Token not visible anymore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,22 @@ import org.javacord.api.DiscordApi
 import org.javacord.api.DiscordApiBuilder
 import org.javacord.api.entity.intent.Intent
 import org.javacord.api.event.message.MessageCreateEvent
+import java.io.File
+import java.io.InputStream
+
+fun getBotToken() : String{
+    val txtFile = "C:/Users/skale/IdeaProjects/JungerLooterBot/botToken.txt"
+    val inputStream: InputStream = File(txtFile).inputStream()
+    val inputString = inputStream.bufferedReader().use {it.readText()}
+
+    return inputString
+}
 
 fun main() {
+
+
     val api: DiscordApi = DiscordApiBuilder()
-        .setToken("MTE4MDI4NjU3NzkxODg3NzgyNw.Gh1tby.lSAXV9nLI5FmR2slES0jAdGqt1gXGxD50b-zu0")
+        .setToken(getBotToken())
         .addIntents(
             Intent.GUILDS,           // Required for bot to be aware of servers it's in
             Intent.GUILD_MESSAGES,    // Required for bot to receive message events
@@ -17,7 +29,6 @@ fun main() {
         if (event.messageContent.equals("!ping", ignoreCase = true)) {
             event.channel.sendMessage("Pong!")
         }
-
     }
 
     println(api.createBotInvite())
